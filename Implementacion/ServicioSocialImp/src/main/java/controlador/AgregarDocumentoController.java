31ff00394c582aa142b9f0ba55eaca5b65635ee9
Victor Niño <ninomtz.victor@gmail.com>
2019-06-10 20:38:12 -0500

    Metodo subir documento

--- Implementacion/ServicioSocialImp/src/main/java/controlador/AgregarDocumentoController.java
+++ Implementacion/ServicioSocialImp/src/main/java/controlador/AgregarDocumentoController.java
@@ -5,22 +5,43 @@
  */
 package controlador;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextArea;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 import modelo.Inscripcion;
 
 /**
@@ -76,13 +97,46 @@ public class AgregarDocumentoController implements Initializable {
                 = FXCollections.observableArrayList(listaMes);
         cbTipoDoc.setItems(obsevableListaTipoOpe);
     }
-    
+    private File archivo;
     @FXML
     private void clicBtnSubirDocumento() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Resource File");
         Stage stage = new Stage();
-        fileChooser.showOpenDialog(stage);
+        archivo = fileChooser.showOpenDialog(stage);
+        lbArchivoAdjunto.setText(archivo.getName());
+        
+    }
+    
+    public File guardarDocumento(){
+        Path origen = Paths.get(archivo.getAbsolutePath());
+        JOptionPane.showMessageDialog(null,"metodo: "+  archivo.getPath());
+        Path destino = Paths.get("src/main/resources/fxml/documentos/" + archivo.getName());
+        try {
+            Files.copy(origen, destino,StandardCopyOption.REPLACE_EXISTING);
+        } catch(IOException e) {
+            return null;
+        }
+        return destino.toFile();
+    }
+    
+    @FXML
+    private void clicBtGuardar() {
+       if(archivo != null) {
+           File copiaArchivo = guardarDocumento();
+           if(copiaArchivo != null) {
+               
+           }else {
+               System.out.println("Error al copiar el archivo, no se pudo realizar el guardoado");
+               
+           }
+       }else {
+           Alert alert = new Alert(Alert.AlertType.WARNING);
+           alert.setTitle("Advertencia");
+           alert.setHeaderText("Agregue un documento antes de guardar");
+           alert.showAndWait();
+       }
+       
     }
     
     @Override
