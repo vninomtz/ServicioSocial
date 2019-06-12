d347ac20e163997c898cbe6d5f3807a7626a039b
Victor Niño <ninomtz.victor@gmail.com>
2019-06-12 14:37:21 -0500

    Pruebas unitarias

---
+++ Implementacion/ServicioSocialImp/src/test/java/PruebasModelo/ModeloTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PruebasModelo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Vik-t
+ */
+public class ModeloTest {
+    
+    public ModeloTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+    public void hello() {
+         assertEquals(true, true);
+    }
+}
